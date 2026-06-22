d4d471ecdd9e3633cb93112ded85246fca9e9a6b
ahmprgr <ahm.prgr.01bin.dev.89@gmail.com>
2026-06-22 12:25:38 +0330

    It is now possible to delete a profile/cover when validation is unsuccessful.

--- src/middlewares/validator.js
+++ src/middlewares/validator.js
@@ -1,14 +1,20 @@
+const path = require("path");
+const fs = require("fs");
+
 const validator = (schema) => {
   return async (req, res, next) => {
     try {
       if (req.file) {
-        req.body.cover = req.file.filename
+        req.body.cover = req.file.filename;
       }
       const result = await schema.validate(req.body, {
         abortEarly: false,
       });
       next();
     } catch (err) {
+      fs.unlink(req.file.path, (e) => {
+        if (e) console.log(e);
+      });
       return res.status(500).json({ message: err.errors });
     }
   };
